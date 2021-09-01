e788bc2fede69d9e64db30f8760b24f0732a6e6e
QuimRobres <quim.robres@gmail.com>
2021-09-01 10:51:35 +0200

    functionality done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React from "react";
+import { useTranslation } from "react-i18next";
+import i18next from "i18next";
+
+const languages = [
+  {
+    code: "cat",
+    name: "Catalan",
+    country_code: "cat",
+  },
+  {
+    code: "esp",
+    name: "Spanish",
+    country_code: "es",
+  },
+  {
+    code: "eng",
+    name: "English",
+    country_code: "gb",
+  },
+];
 
 function App() {
+  const { t } = useTranslation();
+
   return (
     <div className="App">
-      Hello World
+      <div>
+        <ul>
+          {languages.map(({ code, name, country_code }) => (
+            <li key={country_code}>
+              <button onClick={() => i18next.changeLanguage(code)}>
+                <p>{name}</p>
+              </button>
+            </li>,
+            <li key={country_code}>
+              <button onClick={() => i18next.changeLanguage(code)}>
+                <p>{name}</p>
+              </button>
+            </li>,
+            <li key={country_code}>
+              <button onClick={() => i18next.changeLanguage(code)}>
+                <p>{name}</p>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <h1>{t("welcome_message")}</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
+
+import "./index.css";
+import App from "./App";
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs: ["en", "cat", "esp"],
+    fallbackLng: "en",
+    detection: {
+      order: ["cookie", "htmlTag", "localStorage", "path", "subdomain"],
+      caches: ["cookie"],
+    },
+    backend: {
+      loadPath: "/assets/locales/{{lng}}//translation.json",
+    },
+  });
+
+const loadingMarkup = (
+  <div>
+    <p>Loading...</p>
+  </div>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Suspense fallback={loadingMarkup}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>,
+
+  document.getElementById("root")
 );
